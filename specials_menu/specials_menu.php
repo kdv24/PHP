58e4197e13de7093657f0d62d5faf497368be108
Kelly de Vries <kdv@me.com>
2015-01-23 23:12:07 -0800

    Lots more CSS and text.  Added sections for home, menu, and specials pages to be controlled by JS.

--- specials_menu/specials_menu.php
+++ specials_menu/specials_menu.php
@@ -33,91 +33,140 @@
     </div>
   </div>    
 
-<div class="container">
-  <ul class="nav nav-pills">
-      <li role="presentation" class="active"><a href="#">Home</a></li>
-      <li role="presentation"><a href="#">Menu</a></li>
-      <li role="presentation"><a href="#">Specials</a></li>
-  </ul>
-</div>  
-
-  <div class="container">  
-    <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="sunday caption">
-            <h3>Sunday</h3>
-            <p>Ham &amp Cheese Quiche <br> Ginger Peach Mimosa <br> Dark Chocolate Latte</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="monday caption">
-            <h3>Monday</h3>
-            <p>Spinach &amp Ricotta Panini <br> Rosemary Bloody Mary <br> Dark Roast Americano</p>
-          </div>
-        </div>
-      </div>
-
-<div class="clearfix visible-xs-block"></div>
-
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="tuesday caption">
-            <h3>Tuesday</h3>
-            <p>Shrimp Frittata <br> Cranberry Cava Spritzer <br> Cappuccino</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="wednesday caption">
-            <h3>Wednesday</h3>
-            <p>Sun-dried Tomato & Goat Cheese Quiche<br> Milk- yep, just milk <br> Con Panna</p>
-          </div>
-        </div>
-      </div>
+<section id="home_page">
+    <div class="container">
+      <ul class="nav nav-pills">
+          <li role="presentation" class="active"><a href="#">Home</a></li>
+          <li role="presentation"><a href="#">Menu</a></li>
+          <li role="presentation"><a href="#">Specials</a></li>
+      </ul>
+    </div>  
 
 
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="thursday caption">
-            <h3>Thursday</h3>
-            <p>Ham &amp Cheese Quiche <br> Ginger Peach Mimosa <br> Tea of the Day</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="friday caption">
-            <h3>Friday</h3>
-            <p>Avocado &amp Pancetta Wrap <br> Irish Coffee <br> Chai Soy Latte</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="saturday caption">
-            <h3>Saturday</h3>
-            <p>Garden Scramble <br> Grapefruit Fizz <br> Warm Drinking Chocolate</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <div class="thumbnail">
-          <div class="enjoy caption">
-            <h1>Enjoy!</h1>
-          </div>
-        </div>
-      </div>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <img class="img-responsive" src="css/transpcoffee.png" alt="coffee-logo">
+            </div>  
+            <div class="container"> 
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"> 
+                    <div class="contact-info">
+                        <h4>501.123-4567</h4>
+                        <h5>1000 Coffee Ground Lane</h5>
+                        <h5>Portland, OR 97200</h5>   
+                    </div>
+                    <div class="our-story">
+                        <p>Long long ago, in a small village in Italy, our great, great grandparents started brewing the finest coffees around.  It's from this deep heritage that we bring you...<br>
+                        ack, who are we kidding? We are just a couple people who really like our coffee and getting to go out to drink something fun with our breakfast.  We hope you do too!  <br><span class="enjoy">Enjoy!</span> </p>
+                    </div>  
+                   
+                </div> 
+            </div>
+        </div>       
     </div>
-  </div>  
+
+    
+
+</section>
+
+<section id="menu">
+    <div class="container">
+      <ul class="nav nav-pills">
+          <li role="presentation" class="active"><a href="#">Home</a></li>
+          <li role="presentation"><a href="#">Menu</a></li>
+          <li role="presentation"><a href="#">Specials</a></li>
+      </ul>
+    </div>  
+</section>
+
+<section id="specials">
+    <div class="container">
+      <ul class="nav nav-pills">
+          <li role="presentation" class="active"><a href="#">Home</a></li>
+          <li role="presentation"><a href="#">Menu</a></li>
+          <li role="presentation"><a href="#">Specials</a></li>
+      </ul>
+    </div>  
+
+      <div class="container">  
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="sunday caption">
+                <h3>Sunday</h3>
+                <p>Ham &amp Cheese Quiche <br> Ginger Peach Mimosa <br> Dark Chocolate Latte</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="monday caption">
+                <h3>Monday</h3>
+                <p>Spinach &amp Ricotta Panini <br> Rosemary Bloody Mary <br> Dark Roast Americano</p>
+              </div>
+            </div>
+          </div>
+
+    <div class="clearfix visible-xs-block"></div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="tuesday caption">
+                <h3>Tuesday</h3>
+                <p>Shrimp Frittata <br> Cranberry Cava Spritzer <br> Cappuccino</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="wednesday caption">
+                <h3>Wednesday</h3>
+                <p>Sun-dried Tomato & Feta Quiche<br> Milk- yep, just milk <br> Con Panna</p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="thursday caption">
+                <h3>Thursday</h3>
+                <p>Savory Mushroom Tart <br> Blood Orange Bellini <br> Tea of the Day</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="friday caption">
+                <h3>Friday</h3>
+                <p>Avocado &amp Pancetta Wrap <br> Irish Coffee <br> Chai Soy Latte</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="saturday caption">
+                <h3>Saturday</h3>
+                <p>Garden Scramble <br> Grapefruit Fizz <br> Warm Drinking Chocolate</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="thumbnail">
+              <div class="enjoy caption">
+                <h1>Enjoy!</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>  
+</section>
 
 </body>
     <!-- Latest compiled and minified JavaScript -->
